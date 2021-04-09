2bfd5e4c9189d3fe7c89e39a66724e622b80d4e7
Saikat Bishal <saikat.bishal786@gmail.com>
2021-04-09 09:18:59 +0530

    commit - 9-4-21

--- LTI/bit/xor.cpp
+++ LTI/bit/xor.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <cstdlib>
 #include <vector>
 #include <cmath>
+#include <time.h>
+#include <bitset>
 using namespace std;
 #define ll long long
 #define ve vector<int>
-vector<int> makeBinary(int n)
+ve makeBinary(int n)
 {
-    vector<int> v;
+    ve v;
     int digit = 0;
     abs(n);
     while (n > 0)
@@ -18,17 +21,25 @@ vector<int> makeBinary(int n)
     }
     return v;
 }
-void Print(vector<int> v)
+void Print(ve v)
 {
     for (int i = v.size() - 1; i >= 0; i--)
         cout << v[i];
     cout << endl;
 }
-int main()
+
+void checkEven()
 {
-    
+    //check if a number is even or odd
+    srand(time(0));
+    int rNum = 50 + rand() % 101;
+    cout << rNum << " is ";
+    if (rNum & 1)
+        cout << "Odd\n";
+    else
+        cout << "Even\n";
 }
-void func()
+void checkMissing()
 {
 //find the missing number
     int a=0, b=0;
@@ -41,4 +52,44 @@ void func()
     a = a ^ v.size() + 1;
     int ans = a ^ b;
     cout << "missing number = " << ans;
+}
+void checkSigns()
+{
+    int a = 8;
+    int b = 5;
+    int c = -4;
+    cout << bitset<8>(a) << " ";//change decimal to binary with bitset
+    cout << bitset<8>(b) << " ";
+    cout << bitset<8>(c) << " ";
+    if ((a ^ b) < 0)
+        cout << "Different signs\n";
+    else
+        cout << "Same signs\n";
+    if ((a ^ c) < 0)
+        cout << "Different signs\n";
+    else
+        cout << "Same signs\n";
+}
+void checkNeg()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    if ((n ^ 1) < 0)
+/*
+    positive ^ positive = positive;
+    positive ^ negative = negative;
+    negative ^ negative = positive;
+*/
+        cout << "Negative\n";
+    else
+        cout << "Positive\n";
+}
+int main()
+{
+    int t;
+    cout << "Enter test cases : ";
+    cin >> t;
+    while (t--)
+        checkNeg();
 }
